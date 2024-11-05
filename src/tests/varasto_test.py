56e05ebcf69020f111ce752609a2097e83dc381d
kurkilmo <ilmo.kurki00@gmail.com>
2024-11-05 13:04:40 +0200

    Comment tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,6 +5,7 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
+        # :)
         self.neg_varasto = Varasto(-10, -10)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
